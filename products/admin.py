2ee3a05ab7ca3cca78735645189ca9a54976feac
Fathulla Latipov <fathullaxonlatipov@gmail.com>
2024-06-25 19:11:54 +0500

    add product

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
-from .models import CategoryModel
+from .models import CategoryModel, ProductModel
 
-admin.site.register(CategoryModel)
+
+@admin.register(CategoryModel)
+class CategoryModelAdmin(admin.ModelAdmin):
+    search_fields = ['category_name']
+    list_display = ['id', 'category_name', 'created_at']
+    list_filter = ['created_at']
+    ordering = ['-id']
+
+@admin.register(ProductModel)
+class ProductModelAdmin(admin.ModelAdmin):
+    search_fields = ['product_name']
+    list_display = ['id', 'product_name', 'price', 'count', 'created_at']
+    list_filter = ['created_at']
+    ordering = ['-id']
+
+#admin.site.register(CategoryModel)
--- products/models.py
+++ products/models.py
@@ -1,5 +1,44 @@
 from django.db import models
 
 
+# table CategoryModel
+# Column category_name
+# Column2 created_at
+
 class CategoryModel(models.Model):
     category_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class ProductModel(models.Model):
+    product_name = models.CharField(max_length=80)
+    price = models.FloatField()
+    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE, null=True)
+    count = models.IntegerField(default=0)
+    descriptions = models.TextField()
+    image = models.FileField(upload_to='product_image')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_name
+    # pip install pillow
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+
+# Создать модель для Корзины(Заказа)
+# class CartModel
+# user_id
+# user_product -> ForeignKey(ProductModel)
+# user_product_quantity
+# user_add_date -> DateTimeField
+
+# 2.Вы должны показать этот модель на admin.py
