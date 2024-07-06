a5bc61d626621941c4f578d64e409271135a782a
Fathulla Latipov <fathullaxonlatipov@gmail.com>
2024-07-06 19:02:51 +0500

    add search and single product

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from products.models import ProductModel, CategoryModel
 
@@ -8,6 +8,26 @@ def home_page(request):
     categories = CategoryModel.objects.all()
     context = {'products': products, 'categories': categories}
     return render(request, 'index.html', context=context)
+
+def not_fount_page(request):
+    return render(request, 'notfound.html')
+
+def search(request):
+    # Пользотватель отправляет Iphone12
+    if request.method == 'POST':
+        # {'search_product': Iphone12 }
+        get_product = request.POST.get('search_product')
+        try:
+            exact_product = ProductModel.objects.get(product_name__icontains=get_product)
+            return redirect(f'/products/{exact_product.id}')
+        except:
+            return redirect('notfound')
+
+# Страница определенного продукта
+def product_page(request, id):
+    product = ProductModel.objects.get(id=id)
+    context = {'product': product}
+    return render(request, 'single-product.html', context=context)
 
 
 def about_page(request):
--- shop/urls.py
+++ shop/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
-from products.views import home_page, about_page
+from products.views import home_page, about_page, not_fount_page, search, product_page
 from users.views import register_view, login_view, profile_view, logout_view
 
 urlpatterns = [
@@ -15,7 +15,10 @@ urlpatterns = [
     path('signup', register_view, name='signup'),
     path('login', login_view, name='login'),
     path('profile', profile_view, name='profile'),
-    path('logout', logout_view, name='logout')
+    path('logout', logout_view, name='logout'),
+    path('search', search),
+    path('products/<int:id>', product_page),
+    path('notfound', not_fount_page, name='notfound')
 ]
 # /media/product.png,
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
